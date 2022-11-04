75918b72c8f3f5aa532113c2968a30b3397c4a88
SovikPC <vital.gurov2017@gmail.com>
2022-11-04 11:50:32 +0800

    Create activity_info_equipment.xml, InfoEquipActivity.java and InputDialogFragment.java for Update and insert equipment

--- app/src/main/java/com/hfad/inventory/InfoEquipActivity.java
+++ app/src/main/java/com/hfad/inventory/InfoEquipActivity.java
@@ -1,2 +1,7 @@
-package com.hfad.inventory;public class InfoEquipActivity {
+package com.hfad.inventory;
+
+import android.app.Activity;
+
+public class InfoEquipActivity extends Activity {
+
 }
--- app/src/main/java/com/hfad/inventory/InputDialogFragment.java
+++ app/src/main/java/com/hfad/inventory/InputDialogFragment.java
@@ -1,2 +1,21 @@
-package com.hfad.inventory;public class InputDialogFragment {
+package com.hfad.inventory;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class InputDialogFragment extends DialogFragment {
+
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        return builder
+                .setTitle("")
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Отмена", null)
+                .create();
+    }
 }
